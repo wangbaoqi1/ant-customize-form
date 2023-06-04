051b1b70e1cc243c925c6f6a71b7c11dd1587d4d
xiaoyao <1028368131@qq.com>
2023-06-05 00:10:12 +0800

    feat：添加配置

--- .umirc.ts
+++ .umirc.ts
@@ -1,10 +1,15 @@
 import { defineConfig } from 'dumi';
+const repo = 'wbq-dumiapp';
 
 export default defineConfig({
   title: 'wbq-components',
   favicon: '/public/1579967293471.jpeg',
   logo: '/public/1579967293471.jpeg',
-  base: '/public/',
-  publicPath: '/public/',
+  mode: 'site',
+  devServer: {
+    port: 1998 // 自定义端口号
+  },
+  base: process.env.NODE_ENV === 'production' ? `/${repo}/` : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? `/${repo}/` : '/',
   // more config: https://d.umijs.org/config
 });

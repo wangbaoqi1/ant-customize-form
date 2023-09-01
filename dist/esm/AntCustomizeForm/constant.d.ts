61c4f927df63a2f8d40e9689b3070b0b60db2c47
xiaoyao <1028368131@qq.com>
2023-09-01 16:30:08 +0800

    feat: 修改

---
+++ dist/esm/AntCustomizeForm/constant.d.ts
@@ -0,0 +1,24 @@
+/**
+ * 表单类型
+ * 两列以上表单
+ * */
+export declare enum ItemTypes {
+    TITLE = "Title",
+    INPUT = "Input",
+    SELECT = "Select",
+    CASCADER = "Cascader",
+    RADIO = "Radio",
+    TEXTAREA = "TextArea",
+    INPUTNUMBER = "InputNumber",
+    CHECKBOX = "Checkbox",
+    UPLOAD = "Upload",
+    DRAGGER = "Dragger",
+    DATEPICKER = "DatePicker",
+    RANGEPICKER = "RangePicker",
+    TIMEPICKER = "TimePicker",
+    MOREITEM = "MoreItem",
+    TREESELECT = "TreeSelect",
+    BUTTON = "Button",
+    CUSTOMIZE = "Customize",
+    LIST = "List"
+}
---
+++ dist/esm/index.d.ts
@@ -0,0 +1,2 @@
+export { CustomizeForm, ItemTypes, WFC } from './AntCustomizeForm';
+import 'antd/dist/antd.css';
